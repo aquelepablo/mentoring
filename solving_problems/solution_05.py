189abaac3268d497a02ce8c27f847d10f233d390
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2026-03-17 12:10:13 +0100

    fix: improve error handling and improve comparisson

--- solving_problems/solution_05.py
+++ solving_problems/solution_05.py
@@ -3,19 +3,40 @@
 Peça dois números e diga qual deles é o maior ou se são iguais.
 """
 
-print("---| Comparador de Números |---\n")
+from decimal import Decimal
 
-first_number = int(input("Informe o primeiro número: "))
-second_number = int(input("Informe o segundo número: "))
+try:
 
+    print("---| Comparador de Números |---\n")
 
+    first_number = input("Informe o primeiro número: ").strip()
+    second_number = input("Informe o segundo número: ").strip()
 
-if first_number > second_number:
-    print(f"O número {first_number} é maior que o número {second_number}")
+    if not first_number or not second_number:
+        print("Erro: É preciso informar 2 números")
 
-elif first_number == second_number:
-    print(f"O número {first_number} é igual ao número {second_number}")
+    else:
 
-elif first_number < second_number:
-    print(f"O número {second_number} é maior que o número {first_number}")
+        if "." in first_number and "," in first_number:
+            first_number = first_number.replace(".", "")
 
+        if "." in first_number and "," in second_number:
+            second_number = second_number.replace(".", "")
+
+        first_number = first_number.replace(",", ".")
+        second_number = second_number.replace(",", ".")
+
+        first_number = Decimal(first_number)
+        second_number = Decimal(second_number)
+
+        if first_number > second_number:
+            print(f"O número {first_number} é maior que o número {second_number}")
+
+        elif first_number == second_number:
+            print(f"O número {first_number} é igual ao número {second_number}")
+
+        else:
+            print(f"O número {second_number} é maior que o número {first_number}")
+
+except Exception:
+    print("Erro: Deve informar 2 números válidos")
