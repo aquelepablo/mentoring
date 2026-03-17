afde259c2d13d9a4139806dc19b35380f88f4645
aquelePablo <74788322+aquelepablo@users.noreply.github.com>
2026-03-17 11:40:39 +0100

    refactor: improved code

--- solving_problems/solution_04.py
+++ solving_problems/solution_04.py
@@ -3,14 +3,20 @@
 Peça ao usuário um número inteiro qualquer. O programa deve exibir se o número é "Par" ou "Ímpar".
 Dica: Lembre-se do operador de resto da divisão (%). Se o resto da divisão por 2 for zero, o número é par.
 """
+try:
 
-print("---| Verificador de Paridade |---\n")
+    print("---| Verificador de Paridade |---\n")
 
-user_number = int(input("Informe um número inteiro: "))
+    user_number = input("Informe um número inteiro: ").strip().replace(",",".")
 
-parity = "impar"
+    user_number = float(user_number)
+    
+    if not user_number.is_integer():
+        raise Exception("Erro: Deve ser informado um número inteiro para a classificação de pa ou ímpar")
 
-if (user_number%2 == 0):
-    parity = "par"
+    user_number = int(user_number)
 
-print(f"O número {user_number} é {parity}")
+    print(f"O número {user_number} é {'par' if int(user_number) % 2 == 0 else 'ímpar'}")
+
+except Exception as e:
+    print("Erro: Deve ser informado um número válido")
